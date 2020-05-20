8eaf243ba7330c2e6b17cf720b6f464ce6c55ad8
Prameetbisht <myprameet09@gmail.com>
2020-05-20 12:24:34 +0530

    file added

---
+++ Json-API-Parsing.py
@@ -0,0 +1,81 @@
+import urllib.request, urllib.parse, urllib.error, urllib.response
+import urllib.request, urllib.parse, urllib.error
+import json
+import ssl
+
+'''api_key = False
+# If you have a Google Places API key, enter it here
+# api_key = 'AIzaSy___IDByT70'
+# https://developers.google.com/maps/documentation/geocoding/intro
+
+if api_key is False:
+    api_key = 42
+    serviceurl = 'http://py4e-data.dr-chuck.net/json?'
+else :
+    serviceurl = 'https://maps.googleapis.com/maps/api/geocode/json?'
+
+import urllib.error, urllib.request, urllib.parse
+import json
+target = 'http://py4e-data.dr-chuck.net/json?'  #使用这个接口，需要 key参数且值为42
+local = input('Enter location: ')
+url = target + urllib.parse.urlencode({'address': local, 'key' : 42})
+#对字符串进行url编码，直接传递参数和值构成的字典
+print('Retriving', url)
+data = urllib.request.urlopen(url).read()
+print('Retrived', len(data), 'characters')
+js = json.loads(data)
+# print(json.dumps(js, indent = 4)) #查看接收到的文件结构
+print('Place id', js['results'][0]['place_id'])'''
+
+
+import urllib.request, urllib.parse, urllib.error
+import json
+import ssl
+
+api_key = False
+# If you have a Google Places API key, enter it here
+# api_key = 'AIzaSy___IDByT70'
+# https://developers.google.com/maps/documentation/geocoding/intro
+
+if api_key is False:
+    api_key = 42
+    serviceurl = 'http://py4e-data.dr-chuck.net/json?'
+else :
+    serviceurl = 'https://maps.googleapis.com/maps/api/geocode/json?'
+
+# Ignore SSL certificate errors
+ctx = ssl.create_default_context()
+ctx.check_hostname = False
+ctx.verify_mode = ssl.CERT_NONE
+
+while True:
+    address = input('Enter location: ')
+    if len(address) < 1: break
+
+    parms = dict()
+    parms['address'] = address
+    if api_key is not False: parms['key'] = api_key
+    url = serviceurl + urllib.parse.urlencode(parms)
+
+    print('Retrieving', url)
+    uh = urllib.request.urlopen(url, context=ctx)
+    data = uh.read().decode()
+    print('Retrieved', len(data), 'characters')
+
+    try:
+        js = json.loads(data)
+    except:
+        js = None
+
+    if not js or 'status' not in js or js['status'] != 'OK':
+        print('==== Failure To Retrieve ====')
+        print(data)
+        continue
+
+    print(json.dumps(js, indent=4))
+
+    lat = js['results'][0]['geometry']['location']['lat']
+    lng = js['results'][0]['geometry']['location']['lng']
+    print('lat', lat, 'lng', lng)
+    location = js['results'][0]['formatted_address']
+    print(location)
